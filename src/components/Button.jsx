2f595e04f0c32ef61fef18f7393439b965da4c6a
Mohamed <mohamedkondombo135@gmail.com>
2025-08-13 20:02:07 -0400

    feat(header) : add responsive header with active states &CTA

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router";
+
+function Button({ children, variant, className, to }) {
+  const baseStyle =
+    " text-[20px] rounded-[10px] duration-300 transition-all text-neutral-0 px-8 py-4 font-bold leading-[1.4] tracking-[.5px] ";
+
+  const variants = {
+    primary: " bg-neutral-900 hover:bg-neutral-800 ",
+    secondary: " bg-neutral-800",
+    tertiary: " bg-neutral-900 ",
+  };
+  return (
+    <Link to={to} className={`${variants[variant]} ${baseStyle} ${className}`}>
+      {children}
+    </Link>
+  );
+}
+
+export default Button;
---
+++ src/components/Header.jsx
@@ -0,0 +1,101 @@
+import { Link, NavLink } from "react-router";
+import { navLinks } from "../constants";
+import clsx from "clsx";
+import { useState } from "react";
+import Button from "./Button";
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <header className="h-[72px]">
+      {/* container */}
+      <div className="relative p-4 md:px-8 md:pt-8 md:pb-[20px] lg:px-[60px] lg:py-[20px]">
+        <div className="flex items-center justify-between">
+          {/* Logo */}
+          <Link to="/">
+            <img
+              src="/assets/images/logo.svg"
+              alt="logo"
+              className="-ml-4 block h-[32px] w-[242.4px]"
+            />
+          </Link>
+
+          {/* Desktop nav */}
+          <nav className="max-lg:hidden">
+            <ul className="flex gap-10">
+              {navLinks.map(({ path, label }) => (
+                <li key={label} className="group relative">
+                  <NavLink
+                    to={path}
+                    className={({ isActive }) =>
+                      `navLink text-neutral-900 ${isActive}`
+                    }
+                  >
+                    {({ isActive }) => (
+                      <>
+                        {label}
+                        <span
+                          className={`absolute bottom-[2px] left-0 h-[3px] rounded-[4px] bg-orange-500 transition-all duration-500 ${isActive ? "w-full" : "w-0 group-hover:w-full"}`}
+                        />
+                      </>
+                    )}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+
+          {/* Mobile Nav */}
+          <nav
+            className={clsx(
+              "absolute top-full right-0 left-0 z-50 mx-4 overflow-hidden opacity-0 transition-all duration-500 md:mx-8 lg:hidden",
+              isOpen && "opacity-100",
+            )}
+          >
+            <ul
+              className={clsx(
+                "flex flex-col justify-center gap-3 rounded-sm bg-white p-2 transition-all duration-500",
+                isOpen
+                  ? "pointer-events-auto translate-y-0"
+                  : "pointer-events-none -translate-y-full",
+              )}
+            >
+              {navLinks.map(({ path, label }) => (
+                <li key={label} className="group relative">
+                  <NavLink
+                    to={path}
+                    className={({ isActive }) =>
+                      `navLink text-neutral-900 ${isActive}`
+                    }
+                  >
+                    {label}
+                  </NavLink>
+                </li>
+              ))}
+              <Button to="/recipes" className="text-center" variant="primary">
+                Browse recipes
+              </Button>
+            </ul>
+          </nav>
+
+          {/* Hamburger Button */}
+          <button
+            onClick={() => setIsOpen((prevState) => !prevState)}
+            className="flex size-[40px] cursor-pointer items-center justify-center rounded-[4px] bg-neutral-300 lg:hidden"
+          >
+            <img
+              src="/assets/images/icon-hamburger-menu.svg"
+              alt="icon hamburger"
+            />
+          </button>
+
+          <Button className="max-lg:hidden" variant="primary">
+            Browse recipes
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
